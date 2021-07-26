17e08e25de4bca6e9f38fbc25f2a35e79a255de1
Severin Lawson <severinlawson@gmail.com>
2021-07-25 19:26:18 -0500

    weekend1 assignment

---
+++ weekend/api/controllers/country.controller.js
@@ -0,0 +1,183 @@
+const mongoose = require("mongoose");
+const {objectId} = require("mongodb");
+
+const Country = mongoose.model("Country");
+
+module.exports.countriesgetAll = function(req,res){
+    console.log("Json request received");
+    let count =5;
+    let offset =0;
+
+    if(req.query && req.query.count){
+        count = parseInt(req.query.count);
+    }
+
+    if(req.query && req.query.offset){
+        offset = parseInt(req.query.offset)
+    }
+
+    Country.find().skip(offset).limit(count).exec(function(err, countries){
+        console.log("List of countries",countries);
+        res.status(200).json(countries);
+    });
+}
+
+module.exports.countriesgetOne = function(req,res){
+    console.log("Get One countries");
+    const countryId = req.params.countryId;
+    Country.findById(countryId).exec(function(err,country){
+        res.status(200).json(country);
+    })
+}
+
+
+module.exports.countriesAddOne = function(req,res){
+    console.log("AddOne country",req.body);
+   
+        const newCountry = {
+            name : req.body.name,
+            population: parseFloat(req.body.population),
+           
+        };
+        Country.create(newCountry, function(err,createResponse){
+            console.log("ici1",newCountry);
+            const response = {
+                status : 201,
+                message : createResponse
+            }
+            if(err){
+                response.status = 500;
+                response.message = err;
+            }else{
+                response.message = "Country saved";
+            }
+           
+            res.status(response.status).json(response.message);
+        })
+    
+
+}
+
+
+module.exports.countriesFullUpdateOne = function(req,res){
+    console.log("Get One country with full update");
+    const countryId = req.params.countryId;
+    
+    Country.findById(countryId).exec(function(err,country){
+        const response = {
+            status:204,
+            message:country
+        }
+        console.log("the country",country);
+        if(err){
+            console.log("Error finding country");
+            res.status =500;
+            res.message = err;
+            
+        }else if(!country){
+            response.status = 404;
+            response.message = {message: "country with given ID is not found"}
+           
+        }
+        
+        if(response.status !== 204){
+            res.status(response.status).json(response.message);
+        } else{
+            //this is where we update the country
+            country.name =  req.body.name;
+            country.population =  parseInt(req.body.population);
+           
+            country.save(function(err, updatedCountry){
+                if(err){
+                    response.status = 500;
+                    response.message = err;
+                }
+               
+                res.status(response.status).json(response.message);
+                
+            });
+        }
+        
+       
+    })
+   
+    
+}
+
+
+
+module.exports.countriesPartialUpdateOne = function(req,res){
+    console.log("Get One countries");
+    const countryId = req.params.countryId;
+    Country.findById(countryId).exec(function(err,country){
+        console.log("The",country);
+        const response = {
+            status:204,
+            message:country
+        }
+        if(err){
+            console.log("Error finding country");
+            res.status =500;
+            res.message = err;
+            
+        }else if(!country){
+            response.status = 404;
+            response.message = {message: "country with given ID is not found"}
+           
+        }
+        console.log("her");
+        if(response.status !== 204){
+            console.log("her2",response.status);
+            res.status(response.status).json(response.message);
+        } else{
+            
+            
+            if(country.name){country.name =  req.body.name;}
+            
+            if(country.population){country.population = parseInt(req.body.population);}
+           
+
+            country.save(function(err, updatedCountry){
+                if(err){
+                    response.status = 500;
+                    response.message = err;
+                }
+                res.status(response.status).json(response.message);
+                
+            })
+        }
+       // res.status(response.status).json(response.message);
+       
+    })
+   
+    
+}
+
+
+
+module.exports.countriesDeleteOne = function(req,res){
+    console.log("Get One country");
+    const countryId = req.params.countryId;
+    Country.findByIdAndDelete(countryId).exec(function(err,country){
+        const response = {
+            status:200,
+            message:country
+        }
+        if(err){
+            console.log("Error finding country");
+            res.status =500;
+            res.message = err;
+            
+        }else if(!country){
+            response.status = 404;
+            response.message = {message: "country with given ID is not found"}
+           
+        }
+        res.status(response.status).json(response.message);
+       
+    })
+   
+    
+}
+
+
---
+++ weekend/api/controllers/food.controller.js
@@ -0,0 +1,192 @@
+const mongoose = require('mongoose');
+const {objectId} = require("mongodb");
+const Country = mongoose.model("Country");
+
+
+const _addFood = function(req, res, country){
+    
+    const newFood = {
+        name : req.body.name,
+        cookingTime : parseInt(req.body.cookingTime),
+        ingredients : req.body.ingredients,
+        directions : req.body.directions
+    }
+    country.foods.push(newFood);
+
+    country.save(function(err, updatedCountry){
+        const response = {
+            status:204,
+            message : updatedCountry
+        }
+        if (err){
+            response.status = 500;
+            response.message = err;
+        } else {
+            response.message = updatedCountry;
+        }
+        res.status(response.status).json(response.message);
+
+    })
+    
+};
+
+
+
+module.exports.foodsgetAll = function(req,res){
+    console.log("country foods received");
+    const countryId = req.params.countryId;
+    
+
+    Country.findById(countryId).select("foods").exec(function(err, country){
+        console.log("List of country foods",country);
+        res.status(200).json(country.foods);
+    });
+};
+
+module.exports.foodGetOne = function(req,res){
+    const countryId = req.params.countryId;
+    const foodId = req.params.foodId;
+    console.log("get countryId= "+countryId+" foodid= "+foodId);
+    Country.findById(countryId).select("foods").exec(function(err,country){
+       const food = country.foods.id(foodId);
+       console.log(food);
+       res.status(200).json(food);
+   });
+};
+
+
+module.exports.foodAddOne = function (req, res) {
+    console.log("Add one food");
+    const countryId = req.params.countryId;
+    Country.findById(countryId).exec(function(err, country){
+        const response = {
+            status: 204,
+            message: country
+        }
+        if (err){
+            response.status = 500,
+            response.message = err;
+        } else if (!country){
+            console.log("Error ading food");
+            response.status = 404,
+            response.message = {"message": "country ID not found"};
+        } 
+        if (country) {
+            _addFood(req, res, country);
+        } else{
+            res.status(response.status).json(response.message);
+        }
+    })
+   
+};
+
+
+module.exports.foodFullUpdateOne = function(req, res){
+    console.log("Get one country request received");
+    
+    const countryId = req.params.countryId;
+    const foodId = req.params.foodId;
+
+    
+    Country.findById(countryId).exec(function(err, country){
+        const response = {
+            status: 204,
+            message: country
+        }
+        if (err) {
+            console.log("country not updated");
+            response.status = 500;
+            response.message = err;
+        } else if (!country){
+            response.status = 404;
+            response.message = { "message": "country not found" }
+        } if (country) {
+            const food = country.foods.id(foodId);
+            if(food){
+                const foodIndex = country.foods.indexOf(food);
+
+                country.foods[foodIndex] = {
+                    _id : food._id,
+                    name : req.body.name,
+                    cookingTime : parseInt(req.body.cookingTime),
+                    ingredients : req.body.ingredients,
+                    directions : req.body.directions
+                }
+
+                country.save(function(err, updatedCountry){
+                    const response = {
+                        status : 204,
+                        message : updatedCountry
+                    }
+                    if(err){
+                        response.status = 500;
+                        response.message = err;
+                    }
+                    res.status(response.status).json(response.message);
+    
+                })
+            }else {
+                res.status(404).json({"messag": "Food not found."});
+
+            }
+
+
+            //ph
+           
+            
+
+
+            
+        }
+    })
+}
+
+
+
+
+module.exports.foodDeleteOne = function(req, res){
+    console.log("Delete request received");
+   
+    const countryId = req.params.countryId;
+    const foodId = req.params.foodId;
+    Country.findById(countryId).exec(function(err, country){
+        const response = {
+            status: 204,
+            message: country
+        }
+        if (err) {
+            console.log("food not deleted");
+            response.status = 500;
+            response.message = err;
+        } else if (!country){
+            response.status = 404;
+            response.message = { "message": "Publisher not found" }
+        } 
+        if (country) {
+            const food = country.foods.id(foodId);
+
+            if(food){
+                var foodIndx = country.foods.indexOf(food);
+
+                country.foods.splice(foodIndx, 1);
+    
+                country.save(function(err, updatedCountry){
+                    const response = {
+                        status : 204,
+                        message : updatedCountry
+                    }
+                    if(err){
+                        response.status = 500;
+                        response.message = err;
+                    }
+    
+                    res.status(response.status).json(response.message);
+                });
+            }else {
+                res.status(400).json({"message" : "food not found"});
+
+            }
+        }
+        //res.status(response.status).json(response.message);
+    })
+};
