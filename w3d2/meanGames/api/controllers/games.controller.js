207f6f984eb58b969792138310e6fe2bcdc4f7b2
Severin Lawson <severinlawson@gmail.com>
2021-08-03 23:11:21 -0500

    Week 3 day 2 Assignments

--- w3d2/meanGames/api/controllers/games.controller.js
+++ w3d2/meanGames/api/controllers/games.controller.js
@@ -2,37 +2,11 @@
 const mongoose = require("mongoose");
 const Game = mongoose.model("Game");
 
-const runGeoQuery = function(req, res){
-    const lat = parseFloat(req.params.lat);
-    const lng = parseFloat(req.params.lng);
-    console.log(("Geo search lng,lat"),lng,lat);
-
-    const query = {
-        "publisher.location":{
-            $near : {
-                $geometry:{
-                    type: "Point",
-                    coordinates: [lng,lat]
-                },
-                $maxDistance:1000,
-                $minDistance:0
-            }
-        }
-    };
-    Game.find(query).exec(function(err,games){
-        res.json(games);
-    });
-
-};
 
 module.exports.gamesGetAll = function(req, res){
     console.log("Json request received");
 
-    if(req.query && req.query.lat && req.query.lng){
-        runGeoQuery(req,res);
-        return;
-    }
-    
+   
 
     let count = 5;
     let offset = 0;
@@ -48,44 +22,38 @@ module.exports.gamesGetAll = function(req, res){
         res.status(400).json({message:"QueryString offset and count should be a number"});
         return;
     }
+    Game.find().skip(offset).limit(count)
+        .then((games)=>res.status(200).json(games))
+        .catch((err)=> _HamdleError(err,res));
+
    
-    Game.find().skip(offset).limit(count).exec(function(err, games) {
-        if(err){
-            console.log("Error finding games");
-            res.status(500).json(err);
-        } else{
-            console.log("Found games", games);
-            res.status(200).json(games);
-        }
-        
-        });
 }
 
 
 
+
+
+function _HamdlenoGameError(status, message) {
+    const response = {
+      status: status,
+      message: message,
+    };
+    throw response;
+  }
+  
+
+function _HamdleError(err,res){
+    console.log("Error finding game");
+    res.status(404).json(err.message);
+}
+
 module.exports.gamesGetOne = function(req,res){
     console.log("Get One games");
     const gameId = req.params.gameId;
-    Game.findById(gameId).exec(function(err,game){
-        const response = {
-            status:200,
-            message:game
-        }
-        if(err){
-            console.log("Error finding game");
-            res.status =500;
-            res.message = err;
-            
-        }else if(!game){
-            response.status = 404;
-            response.message = {message: "Game with given ID is not found"}
-           
-        }
-        res.status(response.status).json(response.message);
-       
-    })
-   
-    
+    Game.findById(gameId)
+        .then((game)=>res.status(200).json(game))
+        .catch((err)=> _HamdleError(err,res));
+     
 }
 
 module.exports.gamesAddOne = function(req,res){
@@ -102,71 +70,51 @@ module.exports.gamesAddOne = function(req,res){
             designers:req.body.designers,
             publisher:{}
         };
-        Game.create(newGame, function(err,createResponse){
-            console.log("ici1",newGame);
-            const response = {
-                status : 201,
-                message : createResponse
-            }
-            if(err){
-                response.status = 500;
-                response.message = err;
-            }else{
-                response.message = "Game saved";
-            }
-           
-            res.status(response.status).json(response.message);
-        })
-    
-
+        Game.create(newGame)
+            .then((result) => res.status(201).json(result))
+            .catch((err) => _HamdleError(err, res));
+     
 }
+
+   
 
 
     module.exports.gamesFullUpdateOne = function(req,res){
         console.log("Get One games with full update");
         const gameId = req.params.gameId;
-        
-        Game.findById(gameId).exec(function(err,game){
-            const response = {
-                status:204,
-                message:game
-            }
-            console.log("the game",game);
-            if(err){
-                console.log("Error finding game");
-                res.status =500;
-                res.message = err;
-                
-            }else if(!game){
-                response.status = 404;
-                response.message = {message: "Game with given ID is not found"}
-               
-            }
-            
-            if(response.status !== 204){
-                res.status(response.status).json(response.message);
-            } else{
-                game.title =  req.body.title;
-                game.year =  parseInt(req.body.year);
-                game.rate = parseInt(req.body.rate),
-                game.price = parseFloat(req.body.price),
-                game.minPlayers = parseInt(req.body.minPlayers),
-                game.maxPlayers = parseInt(req.body.maxPlayers),
-                game.minAge = parseInt(req.body.minAge),
-                game.designers = [req.body.designer],
-                game.save(function(err, updatedGame){
-                    if(err){
-                        response.status = 500;
-                        response.message = err;
-                    }
+
+        Game.findById(gameId)
+            .then((game)=>{
+                if(!game){
+                    response.status = 404;
+                    response.message = {message: "Game with given ID is not found"}
                    
+                }
+                
+                if(response.status !== 204){
                     res.status(response.status).json(response.message);
-                    
-                });
-            }
-            
-           
-        })
+                } else{
+                    game.title =  req.body.title;
+                    game.year =  parseInt(req.body.year);
+                    game.rate = parseInt(req.body.rate),
+                    game.price = parseFloat(req.body.price),
+                    game.minPlayers = parseInt(req.body.minPlayers),
+                    game.maxPlayers = parseInt(req.body.maxPlayers),
+                    game.minAge = parseInt(req.body.minAge),
+                    game.designers = [req.body.designer],
+                    game.save(function(err, updatedGame){
+                        if(err){
+                            response.status = 500;
+                            response.message = err;
+                        }
+                       
+                        res.status(response.status).json(response.message);
+                        
+                    });
+                }
+            })
+            .catch((err) => _HamdleError(err, res));
+   
        
         
     }
@@ -176,28 +124,18 @@ module.exports.gamesAddOne = function(req,res){
     module.exports.gamesPartialUpdateOne = function(req,res){
         console.log("Get One games");
         const gameId = req.params.gameId;
-        Game.findById(gameId).exec(function(err,game){
-            console.log("The",game);
-            const response = {
-                status:204,
-                message:game
-            }
-            if(err){
-                console.log("Error finding game");
-                res.status =500;
-                res.message = err;
-                
-            }else if(!game){
+
+        Game.findById(gameId)
+        .then((game)=>{
+            if(!game){
                 response.status = 404;
                 response.message = {message: "Game with given ID is not found"}
                
             }
-            console.log("her");
+            
             if(response.status !== 204){
-                console.log("her2",response.status);
                 res.status(response.status).json(response.message);
             } else{
-                
                 if(game.title){game.title =  req.body.title;}
                 console.log("here",req.body.title);
                 if(game.year){game.year =  parseInt(req.body.year);}
@@ -215,39 +153,28 @@ module.exports.gamesAddOne = function(req,res){
                     }
                     res.status(response.status).json(response.message);
                     
-                })
+                });
+
+                
             }
-           // res.status(response.status).json(response.message);
-           
         })
+        .catch((err) => _HamdleError(err, res));
        
         
     }
 
 
 
+    
     module.exports.gamesDeleteOne = function(req,res){
         console.log("Get One games");
         const gameId = req.params.gameId;
-        Game.findByIdAndDelete(gameId).exec(function(err,game){
-            const response = {
-                status:200,
-                message:game
-            }
-            if(err){
-                console.log("Error finding game");
-                res.status =500;
-                res.message = err;
-                
-            }else if(!game){
-                response.status = 404;
-                response.message = {message: "Game with given ID is not found"}
-               
-            }
-            res.status(response.status).json(response.message);
-           
-        })
-       
+        Game.findByIdAndDelete(gameId)
+            .then((game)=>{
+                if (!game) _HamdlenoGameError(404, "game not found");
+                res.status(204).json(game);
+            })
+            .catch((err) => _HamdleError(err, res));     
         
     }
 
--- w3d2/meanGames/api/controllers/users.controller.js
+++ w3d2/meanGames/api/controllers/users.controller.js
@@ -3,11 +3,29 @@ const User = mongoose.model("User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+
+function _HamdleErrorCreate(err,res){
+    console.log("Error creating user");
+    res.status(err.status).json(err.message);
+}
+
+function _HamdleError(err,res){
+    console.log("Error finding user");
+    res.status(err.status).json(err.message);
+}
+
+
+  
+  async function _ComparPasswords(user,password, userpassword) {
+    if (bcrypt.compareSync(password, userpassword)) {
+        console.log("user found", user);
+        let token = jwt.sign({ username: user.username }, "cs572", { expiresIn: 3600 });
+        res.status(200).json({ success: true, token: token });
+    } else { res.status(401).json("Unauthorized"); }
+  }
+
 module.exports.register = function(req,res){
-    console.log("Register user",req.body.username+" "+ req.body.password+"  "+ req.body.name);
-
     
-
     const newUser = {
         username : req.body.username,
         password : bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)) ,
@@ -16,36 +34,29 @@ module.exports.register = function(req,res){
 
     console.log("Register user2", newUser);
 
-    User.create(newUser, function(err,user){
-        if(err){
-            console.log("Error creating user",err);
-            res.status(500).json({message:err});
-        } else{
-            console.log("User created");
-            res.status(201).json(user);
-        }
-    })
+    User.create(newUser)
+        .then(()=>res.status(201).json(user))
+        .catch((err)=> _HamdleErrorCreate(err,res));
+
+   
 }
 
 module.exports.login = (req, res) => {
     console.log("Loging user");
     let username = req.body.username;
     let password = req.body.password;
-    User.findOne({ username: username }).exec((err, user) => {
-        if (err) {
-            console.log(err);
-            res.status(400).json(err);
-        }
-        if (user) {
-            if (bcrypt.compareSync(password, user.password)) {
-                console.log("user found", user);
-                let token = jwt.sign({ username: user.username }, "cs572", { expiresIn: 3600 });
-                res.status(200).json({ success: true, token: token });
-            } else { res.status(401).json("Unauthorized"); }
-        } else {
-            console.log("user not found", user);
-            res.status(400).json("Unauthorized");
-        }
-    });
+
+    User.findOne({ username: username })
+        .then((user)=>{
+            if (user) {
+                _ComparPasswords(user,password, user.password)
+            } else {
+                console.log("user not found", user);
+                res.status(400).json("Unauthorized");
+            }
+        })
+        .catch((err)=> _HamdleError(err,res));
+
+    
 };
 
