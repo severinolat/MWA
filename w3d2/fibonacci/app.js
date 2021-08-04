79039e3285a6a84a4aa4e9ee27c8f3f098abd787
Severin Lawson <severinlawson@gmail.com>
2021-08-04 18:54:55 -0500

    Week 3 day 2 Assignments

--- w3d2/fibonacci/app.js
+++ w3d2/fibonacci/app.js
@@ -2,10 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const child_process = require("child_process");
 const app = express();
+const fib = require("./fibonacci");
 app.set("port",process.env.PORT);
 
+fibonacciPromise = function(number) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            let retVal = fib(number);
+            resolve(retVal);
+        }, 3000);
+    });
+}
+
 console.log("Show 1");
-const newProcess = child_process.spawn("node",["./fibonacci"],{stdio:"inherit"});
+
+fibonacciPromise(41).then((ret) => {
+    console.log(ret);
+});
+
 console.log("Show 2");
 
 const server = app.listen(app.get("port"),function(){
--- w3d2/fibonacci/fibonacci/index.js
+++ w3d2/fibonacci/fibonacci/index.js
@@ -9,6 +9,5 @@ const fib = function (number){
         return fib(number - 1) + fib(number  - 2);
     }
 }
-console.log("Fibonacci of 30 is "+fib(30));
 
-console.log("Fibonacci of -10 is "+fib(-10));
+module.exports = fib ;
